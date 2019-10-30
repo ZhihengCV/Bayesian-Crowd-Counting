d71170ea55d731c322942a228e24eee55d7ccddf
Zhiheng_Ma <469169651@qq.com>
2019-10-30 20:53:53 +0800

    Create crowd.py

---
+++ datasets/crowd.py
@@ -0,0 +1,80 @@
+from PIL import Image
+import torch.utils.data as data
+import os
+from glob import glob
+import torch
+import torchvision.transforms.functional as F
+from torchvision import transforms
+import random
+import numpy as np
+
+def random_crop(im_h, im_w, crop_h, crop_w):
+    res_h = im_h - crop_h
+    res_w = im_w - crop_w
+    i = random.randint(0, res_h)
+    j = random.randint(0, res_w)
+    return i, j, crop_h, crop_w
+
+
+class Crowd(data.Dataset):
+    def __init__(self, root_path, crop_size,
+                 downsample_ratio, is_gray=False,
+                 method='train'):
+
+        self.root_path = root_path
+        self.im_list = sorted(glob(os.path.join(self.root_path, '*.jpg')))
+        if method not in ['train', 'val']:
+            raise Exception("not implement")
+        self.method = method
+
+        self.c_size = crop_size
+        self.d_ratio = downsample_ratio
+        assert self.c_size % self.d_ratio == 0
+        self.dc_size = self.c_size // self.d_ratio
+
+        if is_gray:
+            self.trans = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            self.trans = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+
+    def __len__(self):
+        return len(self.im_list)
+
+    def __getitem__(self, item):
+        img_path = self.im_list[item]
+        gd_path = img_path.replace('jpg', 'npy')
+        img = Image.open(img_path).convert('RGB')
+        if self.method == 'train':
+            keypoints = np.load(gd_path)
+            return self.train_transform(img, keypoints)
+        elif self.method == 'val':
+            keypoints = np.load(gd_path)
+            img = self.trans(img)
+            name = os.path.basename(img_path).split('.')[0]
+            return img, len(keypoints), name
+
+    def train_transform(self, img, keypoints):
+        wd, ht = img.size
+        st_size = 1.0 * min(wd, ht)
+        assert st_size >= self.c_size
+        assert len(keypoints) > 0
+        i, j, h, w = random_crop(ht, wd, self.c_size, self.c_size)
+        img = F.crop(img, i, j, h, w)
+        keypoints = keypoints - [j, i]
+        idx_mask = (keypoints[:, 0] >= 0) * (keypoints[:, 0] <= w) * \
+                   (keypoints[:, 1] >= 0) * (keypoints[:, 1] <= h)
+        keypoints = keypoints[idx_mask]
+        if len(keypoints) > 0:
+            if random.random() > 0.5:
+                img = F.hflip(img)
+                keypoints[:, 0] = w - keypoints[:, 0]
+        else:
+            if random.random() > 0.5:
+                img = F.hflip(img)
+        return self.trans(img), torch.from_numpy(keypoints.copy()).float(), st_size
